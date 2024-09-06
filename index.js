730d3229c8b9491603044275588cfa68b4cab1d7
shemaroger <shemaroger60@gmail.com>
2024-09-06 11:15:33 +0200

    UPDATE

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const swaggerOptions = {
     components: {
       securitySchemes: {
         bearerAuth: {
-          type: 'http',
+          type: 'https',
           scheme: 'bearer',
           bearerFormat: 'JWT',
         },
