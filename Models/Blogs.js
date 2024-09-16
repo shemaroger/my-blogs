76435a6712572c849322c5a4ecf6c9507a7b4b21
shemaroger <shemaroger60@gmail.com>
2024-09-16 10:00:06 +0200

    UPDATE

--- Models/Blogs.js
+++ Models/Blogs.js
@@ -1,15 +1,19 @@
 const mongoose = require("mongoose");
 
-
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   author: { type: String, required: true },
+  image: {
+    type: String,
+    default: '',
+  },
   likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   comments: [{
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     content: { type: String, required: true },
   }],
+  
 });
 
 const Blog = mongoose.model('Blog', blogSchema);
